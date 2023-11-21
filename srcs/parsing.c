53311f2a6614c999e675906967acae05bfbaa422
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2023-11-21 23:02:49 +0100

    added the flood fill

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mevangel <mevangel@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:02:30 by mevangel          #+#    #+#             */
-/*   Updated: 2023/11/21 21:31:59 by mevangel         ###   ########.fr       */
+/*   Updated: 2023/11/21 23:01:40 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,9 @@ static void	ft_find_p_e_c(t_game *game, char *map)
 		}
 	}
 	if (p != 1)
-		ft_error_exit("the game must have only one player starting position", 0);
+		ft_error_exit("the game must have one player starting position", 0);
 	if (e != 1)
-		ft_error_exit("the game must have only one exit", 0);
+		ft_error_exit("the game must have one exit", 0);
 	if (game->coins < 1)
 		ft_error_exit("the game must have at least one collectible", 0);
 }
@@ -66,15 +66,15 @@ static void	ft_check_characters(t_game *game)
 	ft_find_p_e_c(game, game->map);
 }
 
-static void	ft_check_walls(t_game *game)
+static void	ft_check_surrounding_walls(t_game *game)
 {
 	int		x;
 	int		y;
-	char	**tmp;
+	char	**array;
 
 	y = 0;
-	tmp = ft_split(game->map, '\n');
-	if (!tmp)
+	array = ft_split(game->map, '\n');
+	if (!array)
 		ft_error_exit("malloc for split failed.", 1);
 	while (y < game->height)
 	{
@@ -83,16 +83,56 @@ static void	ft_check_walls(t_game *game)
 		{
 			// printf("Checking (%d, %d): %c\n", x, y, tmp[y][x]);
 			if ((y == 0 || y == (game->height - 1) || x == 0 || 
-				x == (game->width - 1)) && tmp[y][x] != '1')
+				x == (game->width - 1)) && array[y][x] != '1')
 			{
-				ft_free_2darr(tmp);
+				ft_free_2darr(array);
 				ft_error_exit("map is not surrounded by walls.", 0);
 			}
 			x++;
 		}
 		y++;
 	}
-	ft_free_2darr(tmp);
+	ft_free_2darr(array);
+}
+
+static void	ft_flood_fill(char *map, int idx, int *coins, int *exit)
+{
+	int	step;
+
+	step = ft_linelen(map) + 1;
+	if (map[idx] == '1')
+		return ;
+	if (map[idx] == 'E')
+	{
+		(*exit)++;
+		return ; //with this line i save the case the exit is before a collectible, but i have problem with exit says unreachable though it is
+	}
+	if (map[idx] == 'C')
+		(*coins)++;
+	map[idx] = '1';
+	ft_flood_fill(map, idx - 1, coins, exit);
+	ft_flood_fill(map, idx + 1, coins, exit);
+	ft_flood_fill(map, idx - step, coins, exit);
+	ft_flood_fill(map, idx + step, coins, exit);
+}
+
+static void	ft_check_player_paths(t_game *game)
+{
+	int		coins;
+	int		exit;
+	char	*tmp_map;
+
+	coins = 0;
+	exit = 0;
+	tmp_map = ft_strdup(game->map);
+	ft_flood_fill(tmp_map, game->player_idx, &coins, &exit);
+	free(tmp_map);
+	if (coins != game->coins && exit != 1)
+		ft_error_exit("exit and collectible(s) not reachable", 0);
+	if (coins != game->coins)
+		ft_error_exit("collectible(s) not reachable", 0);
+	if (exit != 1)
+		ft_error_exit("exit is not reachable", 0);
 }
 
 void	ft_check_given_map(int map_fd, t_game *game)
@@ -119,8 +159,8 @@ void	ft_check_given_map(int map_fd, t_game *game)
 	}
 	free(line);
 	ft_check_characters(game);
-	ft_check_walls(game);
-	// ft_check_player_paths()
+	ft_check_surrounding_walls(game);
+	ft_check_player_paths(game);
 }
 
 
